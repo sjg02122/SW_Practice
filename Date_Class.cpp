30bee3f3a706841f0c3184b0aa81a3139e005619
Jinwoo Bae <33753741+sjg02122@users.noreply.github.com>
2020-10-29 18:40:53 +0900

    Update and rename my_cpp.cpp to Date_Class.cpp

--- Date_Class.cpp
+++ Date_Class.cpp
@@ -2,6 +2,16 @@
 #include <algorithm>
 #include <vector>
 
+/*
+문제 1
+여러분은 아래와 같은 Date 클래스를 디자인 하려고 합니다. 
+SetDate 는 말그대로 Date 함수 내부를 초기화 하는 것이고 AddDay, AddMonth, AddYear 는 일, 월, 년을 원하는 만큼 더하게 됩니다. 
+한 가지 주의할 점은 만일 2012 년 2 월 28 일에 3 일을 더하면 2012 년 2 월 31 일이 되는 것이 아니라 2012 년 3 월 2 일이 되겠지요? 
+(난이도 : 上)
+
+출처 : https://modoocode.com/172
+*/
+
 class Date {
   int year_;
   int month_;  // 1 부터 12 까지.
